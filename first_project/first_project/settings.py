771bf1246ea462ceba5a94b468e7339babd027d0
Mohit <Mohitsonims04@gmail.com>
2023-04-25 01:02:48 +0530

    bug fuxed

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nvl3ssqg9g65ff_-nmcj!$h6dh_vquxf92)0k+w0v48w$vc*il'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
